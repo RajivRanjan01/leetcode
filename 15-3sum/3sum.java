1020585de463b1ce670532cd5a5cc06f9385a489
RajivRanjan01 <139946764+RajivRanjan01@users.noreply.github.com>
2025-07-25 19:28:51 +0530

    Time: 31 ms (56.67%) | Memory: 51.5 MB (57.55%) - LeetSync

--- 15-3sum/3sum.java
+++ 15-3sum/3sum.java
@@ -1,27 +1,44 @@
 class Solution {
-    public List<List<Integer>> threeSum(int[] arr) {
-        if(arr==null || arr.length<3) return new ArrayList<>();
+    public List<List<Integer>> threeSum(int[] nums) {
+        List<List<Integer>> result = new ArrayList<>();
 
-        Arrays.sort(arr);
-        Set<List<Integer>> result = new HashSet<>();
+        Arrays.sort(nums);
 
-        for(int i = 0; i<arr.length-2;i++){
-            int left = i +1;
-            int right = arr.length -1;
-            while(left<right){
-                int sum = arr[i] + arr[left] + arr[right];
-                if(sum ==0){
-                    result.add(Arrays.asList(arr[i],arr[left],arr[right]));
-                    left++;
-                    right--;
-                }else if(sum<0){
-                    left++;
-                }else{
-                    right--;
+        for (int i = 0; i < nums.length - 2; i++) {
+            if (i > 0 && nums[i] == nums[i-1]) {
+                continue;
+            }
+
+            int targetSumForTwoPointers = -nums[i];
+
+            int left_pointer = i + 1;
+            int right_pointer = nums.length - 1;
+
+            while (left_pointer < right_pointer) {
+                // FIX: Changed assignment operator '=' to addition operator '+'
+                int currentSum = nums[left_pointer] + nums[right_pointer];
+
+                if (currentSum == targetSumForTwoPointers) {
+                    result.add(Arrays.asList(nums[i], nums[left_pointer], nums[right_pointer]));
+
+                    while (left_pointer < right_pointer && nums[left_pointer] == nums[left_pointer + 1]) {
+                        left_pointer++;
+                    }
+
+                    while (left_pointer < right_pointer && nums[right_pointer] == nums[right_pointer - 1]) {
+                        right_pointer--;
+                    }
+
+                    left_pointer++;
+                    right_pointer--;
+
+                } else if (currentSum < targetSumForTwoPointers) {
+                    left_pointer++;
+                } else {
+                    right_pointer--;
                 }
             }
-            
         }
-        return new ArrayList<>(result);
+        return result;
     }
 }
