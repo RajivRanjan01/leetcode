c163936f0135140d9775f19caca6c49edaa4dae3
RajivRanjan01 <139946764+RajivRanjan01@users.noreply.github.com>
2025-07-28 01:05:09 +0530

    Time: 0 ms (100.00%) | Memory: 41.6 MB (61.84%) - LeetSync

--- 79-word-search/word-search.java
+++ 79-word-search/word-search.java
@@ -1,38 +1,65 @@
 class Solution {
     public boolean exist(char[][] board, String word) {
-        int m = board.length;
-        int n = board[0].length;
-
-        for (int r = 0; r < m; r++) {
-            for (int c = 0; c < n; c++) {
-                if (board[r][c] == word.charAt(0)) {
-                    if (dfs(board, word, r, c, 0)) {
-                        return true;
-                    }
-                }
+        int m = board.length, n = board[0].length;
+        if (m*n < word.length())
+            return false;
+        char[] wrd = word.toCharArray();
+        int[] boardf = new int[128];
+        for (int i = 0; i < m; ++i)
+        {
+            for (int j = 0; j < n; ++j)
+            {
+                ++boardf[board[i][j]];
+            }
+        }
+        for (char ch : wrd)
+        {
+            if (--boardf[ch] < 0)
+            {
+                return false;
+            }
+        }
+        if (boardf[wrd[0]] > boardf[wrd[wrd.length - 1]])
+            reverse(wrd);
+        for (int i = 0; i < m; ++i)
+        {
+            for (int j = 0; j < n; ++j)
+            {
+                if (wrd[0] == board[i][j]
+                    && found(board, i, j, wrd, new boolean[m][n], 0))
+                    return true;
             }
         }
         return false;
     }
 
-    private boolean dfs(char[][] board, String word, int r, int c, int index) {
-        if (index == word.length()) {
+    private void reverse(char[] word)
+    {
+        int n = word.length;
+        for (int i = 0; i < n/2; ++i)
+        {
+            char temp = word[i];
+            word[i] = word[n - i - 1];
+            word[n - i - 1] = temp;
+        }
+    }
+    private static final int[] dirs = {0, -1, 0, 1, 0};
+    private boolean found(char[][] board, int row, int col, char[] word,
+                        boolean[][] visited, int index)
+    {
+        if (index == word.length)
             return true;
-        }
-
-        if (r < 0 || r >= board.length || c < 0 || c >= board[0].length || board[r][c] != word.charAt(index)) {
+        if (row < 0 || col < 0 || row == board.length || col == board[0].length
+            || board[row][col] != word[index] || visited[row][col])
             return false;
+        visited[row][col] = true;
+        for (int i = 0; i < 4; ++i)
+        {
+            if (found(board, row + dirs[i], col + dirs[i + 1],
+                word, visited, index + 1))
+                return true;
         }
-
-        char originalChar = board[r][c];
-        board[r][c] = '#'; // Mark as visited
-
-        boolean found = dfs(board, word, r + 1, c, index + 1) ||
-                        dfs(board, word, r - 1, c, index + 1) ||
-                        dfs(board, word, r, c + 1, index + 1) ||
-                        dfs(board, word, r, c - 1, index + 1);
-
-        board[r][c] = originalChar; // Backtrack: unmark as visited
-        return found;
+        visited[row][col] = false;
+        return false;
     }
 }
