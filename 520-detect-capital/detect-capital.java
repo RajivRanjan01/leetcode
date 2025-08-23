6a0909b5179f139587882158435422db2fb1b7fb
RajivRanjan01 <139946764+RajivRanjan01@users.noreply.github.com>
2025-08-23 16:22:07 +0530

    Time: 2 ms (37.67%) | Memory: 42.1 MB (25.05%) - LeetSync

--- 520-detect-capital/detect-capital.java
+++ 520-detect-capital/detect-capital.java
@@ -1,12 +1,26 @@
 class Solution {
     public boolean detectCapitalUse(String word) {
-        boolean isAllUppercase = word.equals(word.toUpperCase());
-        boolean isCapitalized = word.matches("[A-Z][a-z]*");
-        boolean isAllLowercase = word.equals(word.toLowerCase());
-
-        if(isAllUppercase || isCapitalized || isAllLowercase)
+        int n = word.length();
+        if (n == 0) {
             return true;
-        else 
-            return false;
+        }
+        
+        if (word.equals(word.toUpperCase())) {
+            return true;
+        }
+
+        if (word.equals(word.toLowerCase())) {
+            return true;
+        }
+        if (Character.isUpperCase(word.charAt(0))) {
+            for (int i = 1; i < n; i++) {
+                if (Character.isUpperCase(word.charAt(i))) {
+                    return false; 
+                }
+            }
+            return true;
+        }
+
+        return false;
     }
 }
