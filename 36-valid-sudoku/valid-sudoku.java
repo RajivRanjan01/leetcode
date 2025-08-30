a3e1dff439c4cf4c3de3e7b05484890eda7fe446
RajivRanjan01 <139946764+RajivRanjan01@users.noreply.github.com>
2025-08-30 17:12:27 +0530

    Time: 2 ms (77.42%) | Memory: 44.4 MB (78.74%) - LeetSync

---
+++ 36-valid-sudoku/valid-sudoku.java
@@ -0,0 +1,34 @@
+import java.util.HashSet;
+
+class Solution {
+    public boolean isValidSudoku(char[][] board) {
+        HashSet<Character>[] rows = new HashSet[9];
+        HashSet<Character>[] cols = new HashSet[9];
+        HashSet<Character>[] boxes = new HashSet[9];
+
+        for (int i = 0; i < 9; i++) {
+            rows[i] = new HashSet<>();
+            cols[i] = new HashSet<>();
+            boxes[i] = new HashSet<>();
+        }
+
+        for (int r = 0; r < 9; r++) {
+            for (int c = 0; c < 9; c++) {
+                char currentVal = board[r][c];
+
+                if (currentVal == '.') {
+                    continue;
+                }
+                int boxIndex = (r / 3) * 3 + (c / 3);
+                if (!rows[r].add(currentVal) || 
+                    !cols[c].add(currentVal) || 
+                    !boxes[boxIndex].add(currentVal)) {
+               
+                    return false;
+                }
+            }
+        }
+
+        return true;
+    }
+}
