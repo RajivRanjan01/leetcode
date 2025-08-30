4a4372234e60666a1d116fca2e050cdc4289992b
RajivRanjan01 <139946764+RajivRanjan01@users.noreply.github.com>
2025-08-30 17:15:06 +0530

    Time: 3 ms (52.39%) | Memory: 44.3 MB (89.38%) - LeetSync

--- 36-valid-sudoku/valid-sudoku.java
+++ 36-valid-sudoku/valid-sudoku.java
@@ -1,34 +1,31 @@
-import java.util.HashSet;
-
 class Solution {
     public boolean isValidSudoku(char[][] board) {
         HashSet<Character>[] rows = new HashSet[9];
-        HashSet<Character>[] cols = new HashSet[9];
-        HashSet<Character>[] boxes = new HashSet[9];
+        
+        HashSet<Character>[] col = new HashSet[9];
+        
+        HashSet<Character>[] box = new HashSet[9];
 
-        for (int i = 0; i < 9; i++) {
+        for(int i = 0 ; i<9 ; i++){
             rows[i] = new HashSet<>();
-            cols[i] = new HashSet<>();
-            boxes[i] = new HashSet<>();
+            col[i] = new HashSet<>();
+            box[i] = new HashSet<>();
         }
 
-        for (int r = 0; r < 9; r++) {
-            for (int c = 0; c < 9; c++) {
+        for(int r = 0 ; r < 9 ; r++){
+            for(int c = 0 ; c < 9 ; c++){
                 char currentVal = board[r][c];
 
-                if (currentVal == '.') {
-                    continue;
-                }
-                int boxIndex = (r / 3) * 3 + (c / 3);
-                if (!rows[r].add(currentVal) || 
-                    !cols[c].add(currentVal) || 
-                    !boxes[boxIndex].add(currentVal)) {
-               
+                if(currentVal == '.') continue;
+
+                int boxIdx = (r/3) * 3 + (c/3);
+
+                if(!rows[r].add(currentVal)||!col[c].add(currentVal)||
+                !box[boxIdx].  add(currentVal)){
                     return false;
                 }
             }
         }
-
         return true;
     }
 }
